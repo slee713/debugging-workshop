d5128b2f808c13eb81d455d13e5d494624902cec
slee713 <steven.lee1393@gmail.com>
2020-08-27 19:43:04 -0400

    this lab sucks

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
+
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
   let joke;
+  //  
+  // function fetchJoke(){
+  //   fetch('https://icanhazdadjoke.com/', {
+  //     method: "GET",
+  //     headers: {
+  //       "Accept": "application/json"
+  //     }
+  //   })
+  //   .then(res => res.json())
+  //   .then(jokeData => joke = jokeData.joke)
+  // }
+  
+  // form.addEventListener('submit', () => {
+  //   const username = document.getElementById('name-input').value
+  //   const newJokeLi = document.createElement('li')
+  //   if (username.lenth > 0) {
+  //     let joke = fetchJoke()
+  //     newJokeLi.innerHTML = `
+  //     <span class="username">${username} says:</span> ${joke}
+  //     `
+  //     jokeList.appendChild(newJokeLi)
+      
+  //   }
+  // })
 
-  function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+  form.addEventListener("submit", ()=> {
+    const username = document.getElementById("name-input").value
+    config = {
+      method: "GET",
       headers: {
         "Accept": "application/json"
       }
-    })
-    .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
-  }
+    }
+    fetch('https://icanhazdadjoke.com/', config)
+    .then(resp => resp.json())
+    .then(jokeData =>  postJoke(jokeData.joke))
+    
+    function postJoke(joke) {
+      let li = document.createElement('li')
+      li.innerText = `<span class="username"> ${username} says: </span> ${joke}`
+      jokeList.append(li)
+    }
 
-  form.addEventListener('submit', (event) => {
 
-    if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
   })
+
+  
+
+
 })
