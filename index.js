8a91fe5c488afd2d3d4ac31323029576829caa68
slee713 <steven.lee1393@gmail.com>
2020-08-28 14:21:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,55 +1,31 @@
-
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  let joke;
-  //  
-  // function fetchJoke(){
-  //   fetch('https://icanhazdadjoke.com/', {
-  //     method: "GET",
-  //     headers: {
-  //       "Accept": "application/json"
-  //     }
-  //   })
-  //   .then(res => res.json())
-  //   .then(jokeData => joke = jokeData.joke)
-  // }
   
-  // form.addEventListener('submit', () => {
-  //   const username = document.getElementById('name-input').value
-  //   const newJokeLi = document.createElement('li')
-  //   if (username.lenth > 0) {
-  //     let joke = fetchJoke()
-  //     newJokeLi.innerHTML = `
-  //     <span class="username">${username} says:</span> ${joke}
-  //     `
-  //     jokeList.appendChild(newJokeLi)
-      
-  //   }
-  // })
-
-  form.addEventListener("submit", ()=> {
-    const username = document.getElementById("name-input").value
-    config = {
+  
+  function fetchJoke(){
+    return fetch('https://icanhazdadjoke.com/', {
       method: "GET",
       headers: {
         "Accept": "application/json"
       }
-    }
-    fetch('https://icanhazdadjoke.com/', config)
-    .then(resp => resp.json())
-    .then(jokeData =>  postJoke(jokeData.joke))
-    
-    function postJoke(joke) {
-      let li = document.createElement('li')
-      li.innerText = `<span class="username"> ${username} says: </span> ${joke}`
-      jokeList.append(li)
-    }
-
-
-  })
-
+    })
+    .then(res => res.json())
+  }
   
-
-
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const username = document.getElementById('name-input').value
+    
+    if(username === "") return;
+    fetchJoke().then(jokeData => {
+      const newJokeLi = document.createElement('li')
+      newJokeLi.innerHTML = `
+      <span class="username">${username} says:</span> ${jokeData.joke}
+      `
+      jokeList.appendChild(newJokeLi)
+      form.reset()
+      
+    })
+  })
 })
